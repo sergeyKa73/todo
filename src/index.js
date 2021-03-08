dc8305652d51d98d88f9bb3b78f57f68498b5344
Sergey <karandeev73@gmail.com>
2021-03-08 22:30:19 +0300

    feat: add components

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-console.log("Hello world!")
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const TodoList = () => {
+    return (
+        <ul>
+            <li>Learn Raact</li>
+            <li>Build Awesome App</li>
+        </ul>
+    )
+}
+
+const AppHeader = () => {
+    return (
+        <h1>My Todo List</h1>
+    )
+}
+
+const SearhPanel = () => {
+    return (
+        <input placeholder="search" />
+    )
+}
+
+const App = () => {
+    return (
+        <div>
+            <AppHeader />
+            <SearhPanel />
+            <TodoList />
+        </div>
+    )
+}
+
+ReactDom.render(<App />, document.getElementById('root'));
